29eff10d2b7bfdfefcd0f2c252381a29afeb2b1a
marcjerome <mjt.eight0five@gmail.com>
2020-07-07 22:24:23 +0800

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,11 +10,45 @@ class ItemValidationTest(FunctionalTest):
 		self.get_item_input_box().send_keys(Keys.ENTER)
 		
 		
+		# The browser intercepts the request, and does not load the list page
+		self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+		    '#id_text:invalid'
+		))
+		
+		# She starts typing some text for the new item and the error disappears 
+		self.get_item_input_box().send_keys("Buy milk")
+		self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+		    '#id_text:valid'
+		))
+		
+		# And she can submit it successfully
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_now_in_list_table("1: Buy milk")
+		
+		# Perversely, she now decides to submit a second blank list item
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		
+		# Again, the browser will not comply
+		self.wait_for_now_in_list_table('1: Buy milk')
+		self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+		    '#id_text:invalid'
+		))
+		
+		# And she can correct it by filling some text in
+		self.get_item_input_box().send_keys('Make tea')
+		self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+		    '#id_text:valid'
+		))
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_now_in_list_table('1: Buy milk')
+		self.wait_for_now_in_list_table('2: Make tea')
+		
 		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
-		self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
-		"You can't have an empty list item"
-		))
+		#self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
+		#"You can't have an empty list item"
+		#))
+
 		
 		# She tries again with some text for the item, which now  works
 		self.get_item_input_box().send_keys('Buy milk')
@@ -22,16 +56,16 @@ class ItemValidationTest(FunctionalTest):
 		self.wait_for_now_in_list_table('1: Buy milk')
 		
 		# Perversely, she now decides to submit a second blank list item
-		self.get_item_input_box().send_keys(Keys.ENTER)
+		#self.get_item_input_box().send_keys(Keys.ENTER)
 		
 		# She receives a similar warning on the list page
-		self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
-		"You can't have an empty list item"
-		))
+		#self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
+		#"You can't have an empty list item"
+		#))
 		
 		
 		# And she can correct it by filling some text in 
-		self.get_item_input_box().send_keys('Make Tea')
-		self.get_item_input_box().send_keys(Keys.ENTER)
-		self.wait_for_now_in_list_table('1: Buy milk')
-		self.wait_for_now_in_list_table('2: Make Tea')
+		#self.get_item_input_box().send_keys('Make Tea')
+		#self.get_item_input_box().send_keys(Keys.ENTER)
+		#self.wait_for_now_in_list_table('1: Buy milk')
+		#self.wait_for_now_in_list_table('2: Make Tea')
